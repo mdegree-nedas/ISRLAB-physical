602e75969ca4df4fe7de6826eb0d96c605ac4a54
ricdip <riccardodpr@gmail.com>
2021-04-23 00:11:34 +0200

    ros2_redis

---
+++ ros2_redis/ros2/src/ros2_redis/ros2_redis/main.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+from ros2_redis.RedisWrapper import RedisWrapper
+
+
+ROS2_TOPIC = 'from_ros2'
+REDIS_TOPIC = 'to_redis'
+
+
+class Translater(Node):
+    def __init__(self):
+        super().__init__("translater")
+        self.redis = RedisWrapper()
+        self.sub = self.create_subscription(String, ROS2_TOPIC, self.listener_callback, 10)
+
+    def listener_callback(self, msg):
+        self.get_logger().info("Forward message '{}' to redis topic {}".format(msg.data, REDIS_TOPIC))
+        self.redis.publish(REDIS_TOPIC, msg.data)
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    translater = Translater()
+
+    rclpy.spin(translater)
+
+    translater.destroy_node()
+    rclpy.shutdown()
