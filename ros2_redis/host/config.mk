efd60b79dd437ebd9ac7a502df655f9562c134d8
valentinodg <valentino.digiosaffatte@student.univaq.it>
2021-04-27 22:55:39 +0200

    repo refactor

--- ros2_redis/host/config.mk
+++
@@ -1,9 +0,0 @@
-PYTHON = python3
-VENV = $(PYTHON) -m venv
-PIP = $(PYTHON) -m pip
-PIPINSTALL = $(PIP) install
-
-MAIN = echo.py
-REQFILE = requirements.txt
-ENVNAME = venv
-ENV = $(ENVNAME)/bin/activate
--- src/publisher/virt_redis_mirror/virt_redis_mirror/common.py
+++ src/publisher/virt_redis_mirror/virt_redis_mirror/common.py
@@ -1,5 +1,3 @@
-# Config variables
-
-SERVER_ADDR = "redis_server"
+SERVER_ADDR = "redis"
 SERVER_PORT = 6379
 DB = 0
