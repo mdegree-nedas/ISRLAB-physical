5211262e801fd6a5ce9d507b3b175fb01434625d
valentinodg <valentino.digiosaffatte@student.univaq.it>
2021-05-11 22:10:22 +0200

    generator ref

--- src/rcp/main.py
+++ src/rcp/main.py
@@ -9,7 +9,9 @@ def main():
         yml_src = rcp.parser.syntax.yaml_parse_file(yml_arg)
         cfg_dict = rcp.parser.syntax.yaml_to_dict(yml_src)
         cfg_parse = rcp.parser.structure.cfg_validate(cfg_dict)
-        rcp.generator.generate(cfg_dict, cfg_parse)
+        rcp.generator.core.generate(cfg_dict, cfg_parse)
+        rcp.generator.template.generate(cfg_dict, cfg_parse)
+        rcp.generator.interface.generate(cfg_dict, cfg_parse)
 
 
 if __name__ == "__main__":
