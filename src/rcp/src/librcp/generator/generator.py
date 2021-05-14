06ac3cfa090a291979745915caef6ad8d0c0464c
valentinodg <valentino.digiosaffatte@student.univaq.it>
2021-05-14 20:56:21 +0200

    rcp ros twist wrapper interface gen

--- src/rcp/src/librcp/generator/generator.py
+++ src/rcp/src/librcp/generator/generator.py
@@ -2,9 +2,13 @@ from librcp.generator.noros.generator_core import NoRosCoreGenerator
 from librcp.generator.noros.generator_template import NoRosTemplateGenerator
 from librcp.generator.noros.generator_interface import NoRosInterfaceGenerator
 
+from librcp.generator.ros.generator_interface import RosInterfaceGenerator
+
 
 class Generator:
     def __init__(self):
         self.noros_core = NoRosCoreGenerator()
         self.noros_template = NoRosTemplateGenerator()
         self.noros_interface = NoRosInterfaceGenerator()
+
+        self.ros_interface = RosInterfaceGenerator()
--- src/rcp/src/rcp.py
+++ src/rcp/src/rcp.py
@@ -6,12 +6,18 @@ def main():
     yml_args = parseArgs()
     rcp = Rcp()
     for yml_arg in yml_args:
+
         yml_src = rcp.parser.syntax.yaml_parse_file(yml_arg)
         cfg_dict = rcp.parser.syntax.yaml_to_dict(yml_src)
         cfg_parse = rcp.parser.structure.cfg_validate(cfg_dict)
+
         rcp.generator.noros_core.generate(cfg_dict, cfg_parse)
         rcp.generator.noros_template.generate(cfg_dict, cfg_parse)
         rcp.generator.noros_interface.generate(cfg_dict, cfg_parse)
+
+        # rcp.generator.ros_core.generate(cfg_dict, cfg_parse)
+        # rcp.generator.ros_broker.generate(cfg_dict, cfg_parse)
+        rcp.generator.ros_interface.generate(cfg_dict, cfg_parse)
 
 
 if __name__ == "__main__":
